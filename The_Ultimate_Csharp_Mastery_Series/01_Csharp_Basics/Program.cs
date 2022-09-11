18a309e411aff3ac5e917ddfdcd1e53ebd9f650c
Osman Taş <68549050+ta5n@users.noreply.github.com>
2022-09-11 18:49:12 +0300

    add solution and projects

---
+++ The_Ultimate_Csharp_Mastery_Series/01_Csharp_Basics/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _01_C__Basics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ The_Ultimate_Csharp_Mastery_Series/04_Csharp_Coding_Speed/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Csharp_Coding_Speed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
